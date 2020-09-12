92c65a16f7b8ecc217995bc045e0353d0a7aed5c
j.giulianetti <j.giulianetti@gmail.com>
2020-09-12 09:42:52 -0300

    Empezando con algunas clases

---
+++ UNQfy/Track.js
@@ -0,0 +1,18 @@
+class Track {
+    constructor(id,nombre, duracion, generos){
+        this.id = id
+        this.nombre =nombre
+        this.duracion = duracion
+        this.generos = generos
+               
+    }
+
+    //Getters
+    getId(){return this.id}
+    getNombre(){return this.nombre}
+    getGeneros(){return this.generos}
+    getDuracion(){return this.duracion}
+    //Getters
+      
+    
+}
