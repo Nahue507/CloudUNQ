5476693e68cfecd8e172bdac8d69dc3bc319c3bd
j.giulianetti <j.giulianetti@gmail.com>
2020-09-19 23:25:49 -0300

    Clase Playlist lista y probada

--- UNQfy/unqfy.js
+++ UNQfy/unqfy.js
@@ -139,7 +139,7 @@ class UNQfy {
   static load(filename) {
     const serializedData = fs.readFileSync(filename, {encoding: 'utf-8'});
     //COMPLETAR POR EL ALUMNO: Agregar a la lista todas las clases que necesitan ser instanciadas
-    const classes = [UNQfy, Artista, Album, Track, playlist];
+    const classes = [UNQfy, Artista, Album, Track, Playlist];
     return picklify.unpicklify(JSON.parse(serializedData), classes);
   }
 }
